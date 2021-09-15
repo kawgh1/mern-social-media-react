14ebd648662b75c49d7261059b1b52bbc51b7c8c
kawgh1 <krseven@gmail.com>
2021-09-15 15:20:09 -0500

    setting up login and registration pages

---
+++ src/pages/register/Register.js
@@ -0,0 +1,50 @@
+import "./Register.css";
+
+export default function Register() {
+	return (
+		<div className="register">
+			<div className="registerWrapper">
+				<div className="registerLeft">
+					<h3 className="registerLogo">reactsocial</h3>
+					<span className="registerDesc">
+						Connect with friends around the world on{" "}
+						<span className="react-span">reactsocial</span>
+					</span>
+				</div>
+				<div className="registerRight">
+					<div className="registerBox">
+						<input
+							placeholder="Username"
+							className="registerInput"
+						/>
+						<input placeholder="Email" className="registerInput" />
+						<input
+							placeholder="Password"
+							className="registerInput"
+						/>
+						<input
+							placeholder="Confirm Password"
+							className="registerInput"
+						/>
+						<button className="registerButton">
+							<img
+								src="/logo512.png"
+								alt="react"
+								className="logo-img"
+								style={{
+									height: "29px",
+									width: "29px",
+									marginRight: "5px",
+								}}
+							/>
+							Sign Up
+						</button>
+						<button className="registerRegisterButton">
+							Log in
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
